f94d2e53764e4c1ff825ea80c1ed31e02dd97603
alexi cuellar <ALEXI27CUELLAR@hotmail.fr>
2018-06-21 09:50:57 +0200

    Add ISR who check pulse motor every 20us

--- self_balance_robot/self_balance_robot/self_balance_robot.ino
+++ self_balance_robot/self_balance_robot/self_balance_robot.ino
@@ -25,6 +25,8 @@ double gyroXangle, gyroYangle; // Angle calculate using the gyro only
 double compAngleX, compAngleY; // Calculated angle using a complementary filter
 double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
 
+int counter, state;
+
 uint32_t timer;
 uint8_t i2cData[14]; // Buffer for I2C data
 
@@ -38,9 +40,17 @@ void setup() {
 	
 	Serial.begin(115200);
 	Wire.begin();
-	
+	//To create a variable pulse for controlling the stepper motors a timer is created that will execute a piece of codeevery 20us
+	//This routine is TIMER2_COMPA_vect
+	TCCR2A = 0;                                                              
+	TCCR2B = 0;                                                               
+	TIMSK2 |= (1 << OCIE2A);                                                 
+	TCCR2B |= (1 << CS21);                                                  
+	OCR2A = 39;                                                               
+	TCCR2A |= (1 << WGM21);                                                   
+
 	/* Set kalman and gyro starting angle */
-	/*while (i2cRead(0x3B, i2cData, 6));
+	while (i2cRead(0x3B, i2cData, 6));
 	accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
 	accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
 	accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
@@ -48,21 +58,15 @@ void setup() {
 	kalmanX.setAngle(roll); // Set starting angle
 	gyroXangle = roll;
 	timer = micros();
-	*/
+	
 	Serial.print("START");
 }
 
 void loop() {
 
-	PORTB &= 0b11110001;
-	PORTD |= 0b01000000;
-	PORTB |= 0b00010000;
-	delayMicroseconds(500);
-	PORTD &= 0b10111111; ;
-	PORTB &= 0b11101111;
-	delayMicroseconds(500);
+	
 		/* Update all the values */
-	/*	while (i2cRead(0x3B, i2cData, 14));
+		while (i2cRead(0x3B, i2cData, 14));
 		accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
 		accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
 		accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
@@ -96,9 +100,33 @@ void loop() {
 	//Serial.print(roll); Serial.print("\t");
 	//Serial.print(gyroXangle); Serial.print("\t");
 	//Serial.print(kalAngleX); Serial.print("\t");
-
-
-
 	//Serial.print("\r\n");
-	//delay(20);*/
 }
+
+ISR(TIMER2_COMPA_vect){
+  PORTB &= 0b00100001;
+  if (counter < 10) counter++;
+  else{
+    counter =0;
+    if (state==1){
+      PORTD |= 0b01000000;
+      PORTB |= 0b00010000;  }
+               //Set output 2 high to create a pulse for the stepper controller
+    else{ 
+      PORTD &= 0b10111111;
+      PORTB &= 0b11101111; }
+    state =!state;}        //Set output 4 low because the pulse only has to last for 20us
+}
+
+/*
+PORTB &= 0b11110001;
+PORTD |= 0b01000000;
+PORTB |= 0b00010000;
+delayMicroseconds(500);
+PORTD &= 0b10111111; ;
+PORTB &= 0b11101111;
+delayMicroseconds(500);
+*/
+
+
+
